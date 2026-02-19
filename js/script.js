e73403b36dd550eb79a67390fe6d4c019ceda483
valerylamassoliz-glitch <valerylamassoliz@gmail.com>
2026-02-18 23:08:03 -0400

    estructura base

---
+++ js/script.js
@@ -0,0 +1,9 @@
+console.log("Valery Marcela Lamas Soliz");
+
+const boton = document.querySelector("button");
+
+boton.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    
+    alert("¡Ola! Gracias por hacer clic ");
+});
